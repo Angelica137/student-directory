6541cd9466b2f82edc6249d87792f39e7d7858c9
Angelica <Angelica137@users.noreply.github.com>
2020-03-11 20:34:29 +0000

    changed iteration to while loop for prinint name list

--- directory.rb
+++ directory.rb
@@ -1,9 +1,7 @@
 #let the user input names
-
-
 def input_students
   puts  "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "To finish, just hit return twice."
   #create an empty array
   @students = []  
   #get the first name
@@ -11,9 +9,14 @@ def input_students
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    @students << {name: name, cohort: :november}
+#   @students << {name: name, cohort: :november}
+    puts "What are their hobbies?"
+    hobbies = gets.chomp
+    @students <<{name: name, hobbies: hobbies, cohort: :november}
     puts "Now we have #{@students.count} students"
-    #get another name form the user
+    #try to add extra information once student list has been created.
+    puts "Do you want to add another student? To finish hit return twice."
+    #get another name from the user
     name = gets.chomp
   end
   @students
@@ -25,12 +28,22 @@ def print_header
   puts "The students of Villains Academy"
   puts "------------------"
 end
+
+#replaced iterator with while loop. Think iterator is better. Check and amend if 
+#needed
 def print(students)
-  @students.each_with_index() do |student, i|
-    index = i+1
-    puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
+  count = 0
+  while count <= @students.length do
+    puts @students[count]#.key
+    count += 1
   end
+#    @students.each_with_index() do |student, i|
+#     index = i+1
+#      puts "#{index}. for #{student[:name]} (#{student[:cohort]} cohort)"
+#    end
+#  end
 end
+
 #finally, we print the total number of students
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
@@ -41,6 +54,7 @@ def print_first_letter(prefix)
   prefix = gets.chomp
   puts @students.select { |student| student[:name].start_with? prefix}
   #this only works if prefix is with capital, need to fix it so it works for non capitals too
+  #also, what happens if the user wants to quit here?
 end
 
 def print_short_name
@@ -49,7 +63,7 @@ def print_short_name
   if answer == 'yes'
     puts @students.select { |student| student[:name].length < 12}
   else
-    puts nil
+    puts 'bye'
   end
 end
 
