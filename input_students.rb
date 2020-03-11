98dd010deb224837370e667e62f0835d4df3d83e
Angelica <Angelica137@users.noreply.github.com>
2020-03-11 23:58:15 +0000

    add fixes to typos input_students

--- input_students.rb
+++ input_students.rb
@@ -2,39 +2,38 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
-  students = []
+  @students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
   # add the student hash to the array
-    students << {name: name, 
+    @students << {name: name, 
     cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
   # get another name from the user
     name = gets.chomp
   end
   # return the array of students
-  students
+  @students
 end
 
 def print_header
-  put "The students of my cohort at Makers Academy"
-  puts ``-------------``
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
-def print(students)
-  student.each do |student|
+def print(student)
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer names
-  prints "Overall, we have {names.count} great students"
+def print_footer(students)
+  print "Overall, we have #{students.count} great students"
 end
-
 
 students = input_students
 print_header
-print{students}
-print_footer[students]
+print(students)
+print_footer(students)
