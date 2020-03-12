e4f346ab9b35a88dc080dfae54029a7ef39fea11
Angelica <Angelica137@users.noreply.github.com>
2020-03-12 17:05:48 +0000

    add data from file

--- directory.rb
+++ directory.rb
@@ -109,6 +109,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -126,6 +127,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -152,6 +155,15 @@ def save_students
     #write the var csv_line to the file using puts
     #this puts it to the file and not the screen
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
